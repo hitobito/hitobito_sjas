6ec5988160e5e10c08241639d0d221c58c40aafe
Mathis Hofer <hofer@puzzle.ch>
2020-11-11 15:24:26 +0100

    Add place of origin to person #2

---
+++ db/migrate/20201111125039_add_place_of_origin_to_person.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2012-2020, Stiftung für junge Auslandssschweizer. This file is part of
+#  hitobito_sjas and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_sjas.
+
+class AddPlaceOfOriginToPerson < ActiveRecord::Migration[6.0]
+  def change
+    add_column :people, :place_of_origin, :string, default: nil
+  end
+end
