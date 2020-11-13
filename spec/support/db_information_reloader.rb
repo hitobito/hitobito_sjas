4df62a49a4c29585f88b61321d073807c160af02
Matthias Viehweger <viehweger@puzzle.ch>
2020-11-13 12:37:17 +0100

    Remove db-info reloader since its in core now

--- spec/support/db_information_reloader.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-#  Copyright (c) 2020, Stiftung für junge Auslandssschweizer. This file is part of
-#  hitobito_sjas and licensed under the Affero General Public License version 3
-#  or later. See the COPYING file at the top-level directory or at
-#  https://github.com/hitobito/hitobito_sjas.
-
-Person.reset_column_information
