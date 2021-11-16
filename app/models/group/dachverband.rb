773677fa4d13cef25f11375ade37463ddd58b5a5
Matthias Viehweger <viehweger@puzzle.ch>
2021-11-16 16:33:52 +0100

    Activate courses on Dachverband-Layer
    SJAS wants to use courses and qualifications.

--- app/models/group/dachverband.rb
+++ app/models/group/dachverband.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-#  Copyright (c) 2012-2020, Stiftung für junge Auslandschweizer. This file is part of
+#  Copyright (c) 2012-2021, Stiftung für junge Auslandschweizer. This file is part of
 #  hitobito_sjas and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_sjas.
@@ -9,7 +9,7 @@
 # SJAS
 class Group::Dachverband < ::Group
   self.layer = true
-  self.event_types = [Event, Event::Camp]
+  self.event_types = [Event, Event::Course, Event::Camp]
 
   self.default_children = [
     Group::DachverbandGeschaeftsstelle,
