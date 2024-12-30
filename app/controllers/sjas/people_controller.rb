dfd6459a0cd3b3665a0fc9a97df53dc6a5d5e56c
Niklas <68543592+njaeggi@users.noreply.github.com>
2024-12-30 11:15:58 +0100

    Feature/90/new person fields (#91)
    * Add migration
    
    * Include new attributes in permitted and searchable attrs
    
    * Add translations
    
    * Render attrs and add fields in form
    
    * Add person search spec
    
    * Fix rubocop

--- app/controllers/sjas/people_controller.rb
+++ app/controllers/sjas/people_controller.rb
@@ -11,7 +11,8 @@ module Sjas::PeopleController
   included do
     self.permitted_attrs += [
       :place_of_origin, :language_skill_de, :language_skill_fr, :language_skill_it,
-      :language_skill_en, :language_skill_es, :other_native_language
+      :language_skill_en, :language_skill_es, :other_native_language, :bank_account,
+      :insurance_company, :insurance_number
     ]
   end
 end
