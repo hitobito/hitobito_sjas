4252230bc87506322456e0e2abe44f8607e77249
Matthias Viehweger <kronn@kronn.de>
2026-03-20 17:10:28 +0100

    Update seeding of custom contents

--- db/seeds/custom_contents.rb
+++ db/seeds/custom_contents.rb
@@ -5,7 +5,7 @@
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_sjas.
 
-CustomContent.seed_once(:key,
+CustomContent.seed(:key,
   {
     key: Event::ParticipationMailer::CONTENT_CAMP_CONFIRMATION,
     placeholders_required: 'camp-name',
